d730fcaa016414ce7fb22aa40b169042735475ab
Medac <44846888+Medac@users.noreply.github.com>
2019-02-25 21:02:57 -0600

    Adds discord.js library

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-if (msg.content === 'ping') {
-msg.reply('pong');
-}
+// client.on('message', msg => {
+//   if (msg.content === 'w!claim Holo') {
+//     msg.reply('I cannot believe you have done this. Return me to Cade immediately.');
+//   }
+// });
+
+client.on("message", (message) => {
+  if (message.content.startsWith("OwO")) {
+    message.channel.send("Fuck you stop OwOing");
+  } else
+
+    if (message.content.startsWith("w!claim Holo")) {
+      message.channel.send("I cannot believe you have done this. Return me to Cade immediately.");
+    }
 });
 
 client.login('token');
