3c72c4bc3710c38b8edda776d362ef7caca1b904
Medac <44846888+Medac@users.noreply.github.com>
2019-02-26 10:16:12 -0600

    Adds prefix  based commands and bot detection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = 'h!';
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -11,13 +13,30 @@ client.on('ready', () => {
 //   }
 // });
 
-client.on("message", (message) => {
-  if (message.content.startsWith("OwO")) {
-    message.channel.send("Fuck you stop OwOing");
+
+
+//Commands text based
+client.on('message', (message) => {
+  if (message.content.startsWith('OwO')) {
+    message.channel.send('Fuck you stop OwOing');
   } else
 
-    if (message.content.startsWith("w!claim Holo")) {
-      message.channel.send("I cannot believe you have done this. Return me to Cade immediately.");
+    if (message.content.startsWith('w!claim Holo')) {
+      message.channel.send('I cannot believe you have done this. Return me to Cade immediately.');
+    }
+});
+
+
+//Commands with integrated prefix and bot detection
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+    if (message.content.startsWith(prefix + "foo")) {
+      message.channel.send("bar!");
     }
 });
 
