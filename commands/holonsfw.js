b30630d201f1bdc708d27a0c2e45d767f699a950
Medac <44846888+Medac@users.noreply.github.com>
2019-06-10 17:59:54 -0500

    adds nsfw command

--- commands/holonsfw.js
+++ commands/holonsfw.js
@@ -1,13 +1,23 @@
 const { Discord, RichEmbed } = require("discord.js")
+const config = require("../config.json");
 const fs = require('fs')
 
 module.exports.run = async (bot, message, args) => {
-
-  let embed = new RichEmbed()
-    .setImage('https://i.imgur.com/')
-    .setColor('#FF4500')
-
-  message.channel.send(embed);
+  if (message.author.id === config.ownerID) {
+    const embed = new RichEmbed()
+      .setAuthor(`${message.author.username}`, `${message.author.avatarURL}`)
+      .setColor('#FF4500')
+      .setImage("https://i.imgur.com/iFjVL6w.jpg")
+    message.channel.send({ embed });
+  } else if (message.author.id !== config.ownerID) {
+    const embed = new RichEmbed()
+      .setTitle('YOU HAVE ANGERED HOLO!! TASTE BAD FANART SCOUNDREL!')
+      .setAuthor(`${message.author.username}`, `${message.author.avatarURL}`)
+      .setColor('#990000')
+      .setImage("https://i.imgur.com/EgSg11E.jpg")
+      .setTimestamp()
+    message.channel.send({ embed });
+  }
 }
 
 module.exports.help = {
